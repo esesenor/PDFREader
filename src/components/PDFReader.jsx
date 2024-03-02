1ff3a10d30815ab5dda201ca141172cdb5ddbd19
esesenor <rios.esesenor90@gmail.com>
2024-03-01 22:36:58 -0500

    datos

--- src/components/PDFReader.jsx
+++ src/components/PDFReader.jsx
@@ -6,6 +6,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 const PDFReader = () => {
   const [file, setFile] = useState(null);
   const [numerosTransferencia, setNumerosTransferencia] = useState([]);
+  const [numerosRequisicion, setNumerosRequisicion] = useState([]);
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -17,51 +18,73 @@ const PDFReader = () => {
     reader.onload = async (e) => {
       const pdfData = new Uint8Array(e.target.result);
       const pdf = await pdfjs.getDocument({ data: pdfData }).promise;
-      let numeros = [];
+      let numerosTransferencia = [];
+      let numerosRequisicion = [];
       for (let i = 1; i <= pdf.numPages; i++) {
         const page = await pdf.getPage(i);
         const textContent = await page.getTextContent();
         textContent.items.forEach((textItem) => {
           const numeroTransferenciaRegex = /IST-0*(\d+)/g;
-          const matches = textItem.str.matchAll(numeroTransferenciaRegex);
-          for (const match of matches) {
+          const numeroRequisicionRegex = /VRR-0*(\d+)/g;
+          const matchesTransferencia = textItem.str.matchAll(
+            numeroTransferenciaRegex
+          );
+          const matchesRequisicion = textItem.str.matchAll(
+            numeroRequisicionRegex
+          );
+          for (const match of matchesTransferencia) {
             let numero = "IST " + match[1].replace(/^0+/, "");
-            numeros.push(numero);
+            numerosTransferencia.push(numero);
+          }
+          for (const match of matchesRequisicion) {
+            let numero = "VRR " + match[1].replace(/^0+/, "");
+            numerosRequisicion.push(numero);
           }
         });
       }
-      setNumerosTransferencia(numeros);
+      setNumerosTransferencia(numerosTransferencia);
+      setNumerosRequisicion(numerosRequisicion);
     };
     reader.readAsArrayBuffer(file);
   };
 
   return (
-    <div
-      className="flex flex-col justify-center items-center gap-6 -10
-     bg-sky-950 w-full h-full"
-    >
+    <div className="flex flex-col justify-center items-center gap-6 bg-sky-950 w-full h-full">
       <h1 className="text-rose-100 p-4 text-xl">
-        Extraer Números de Transferencia{" "}
+        Extraer Números de Transferencia y Requisición
       </h1>
       <input type="file" onChange={handleFileChange} className="m-10" />
       <button
         onClick={extractNumerosTransferencia}
-        className="bg-rose-300 text-[#020202] w-[300px] 
-        p-4 shadow-sm shadow-gray-300 transition-all text-lg font-bold
-        hover:-translate-y-1 hover:shadow-lg hover:shadow-white"
+        className="bg-rose-300 text-[#020202] w-[300px] p-4 shadow-sm shadow-gray-300 transition-all text-lg font-bold hover:-translate-y-1 hover:shadow-lg hover:shadow-white"
       >
         Extraer Números
       </button>
-      <div className="m-10 text-white flex flex-col justify-center items-center">
-        <h2 className="">Lista Números de Transferencia:</h2>
-        <ul>
-          {numerosTransferencia.map((numero, index) => (
-            <li key={index}>{numero}</li>
-          ))}
-        </ul>
+      <div className="m-10 text-white">
+        <h2 className="mb-2 text-center">
+          Listado de Números de Transferencia y Requisición:
+        </h2>
+        <table className="table-auto text-center w-full">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Número de Transferencia</th>
+              <th className="px-4 py-2">Número de Requisición</th>
+            </tr>
+          </thead>
+          <tbody>
+            {numerosTransferencia.map((transferencia, index) => (
+              <tr key={index}>
+                <td className="border px-4 py-2">{transferencia}</td>
+                <td className="border px-4 py-2">
+                  {numerosRequisicion[index]}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
       {/* {file && (
-        <div style={{ marginTop: "20px" }}>
+        <div className="m-10 w-full">
           <Document file={file}>
             {Array.from(new Array(file.numPages), (el, index) => (
               <Page key={`page_${index + 1}`} pageNumber={index + 1} />
